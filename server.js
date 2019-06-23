cca5a55ff483b679b54dffa35d1b5a2328eb0c7f
Vasily93 <markovavasily@gmail.com>
2019-06-23 00:54:57 -0700

    replaced function with anonymous function

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ http.createServer(function(request, response) {
     response.end();
 }).listen(8888);
 
-function say(word) {
-    console.log(word);
-}
-
 function execute(someFunction, value) {
     someFunction(value);
 }
 
-execute(say, 'Hello');
+execute(function(word) { console.log(word) }, 'Hello');
