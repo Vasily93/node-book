84c49cb8ac91a909a2f3f46b73d711e111b46c9a
Vasily93 <markovavasily@gmail.com>
2019-06-24 00:53:01 -0700

    removed  formData and added requests, finished functionality ti show png

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,10 @@
 // var exec = require('child_process').exec;
 
 var querystring = require('querystring'),
-fs = require('fs');
+fs = require('fs'),
+formidable = require('formidable');
 
-function start(response, postData) {
+function start(response) {
     console.log('Request handler "start" was called.');
 
     var body = '</html>'+
@@ -14,7 +15,7 @@ function start(response, postData) {
     '<body>'+
     '<form action="/upload" enctype="multipart/form-data" '+
     'method="post">'+
-    '<input type="file" name="upload">'+
+    '<input type="file" name="upload" miltiple="multiple">'+
     '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
@@ -25,12 +26,25 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log('Request handler "upload" was called.');
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('you have sent the text:'+
-    querystring.parse(postData).text);
-    response.end();
+    var form = new formidable.IncomingForm();
+    console.log('about to parse');
+    form.parse(request, function(error, fields, files) {
+        console.log('parsing done');
+
+        fs.rename(files.upload.path, '.tmp/test.png', function(error){
+            if (error) {
+                fs.unlink('.tmp/test.png');
+                fs.rename(files.upload.path, '.tmp/test.png')
+            }
+        });
+        
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write('recieved image:<br/>');
+        response.write('<img src="/show" />');
+        response.end();
+    });
 }
 
 function show(response) {
--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 function route(handle, pathname, response, postData) {
     console.log('About to route a sequest for' + pathname);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname](response, postData);
+        handle[pathname](response, request);
     } else {
         console.log('no request handler found for' + pathname);
-        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.writeHead(404, {'Content-Type': 'text/html'});
         response.write('404 Not Found');
         response.end();
     }
