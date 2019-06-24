22e3eb8de44d524b31b2066515fb784658088432
Vasily93 <markovavasily@gmail.com>
2019-06-23 22:55:41 -0700

    refactored html to post data

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,13 @@
-var exec = require('child_process').exec;
+// var exec = require('child_process').exec;
 
-function start(response) {
+var querystring = require('querystring')
+
+function start(response, postData) {
     console.log('Request handler "start" was called.');
 
     var body = '</html>'+
     '<head>'+
-    '<meate http-equiv="Content-Type" content="text.html;'+
+    '<meate http-equiv="Content-Type" content="text/html; '+
     '<charset=UTF-8" />'+
     '</head>'+
     '<body>'+
@@ -16,15 +18,16 @@ function start(response) {
     '</body>'+
     '</html>';
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(body);
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log('Request handler "upload" was called.');
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello Upload');
+    response.write('you have sent the text:'+
+    querystring.parse(postData).text);
     response.end();
 }
 
